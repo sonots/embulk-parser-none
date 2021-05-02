cec4a35642c56cc185dd50bc054ff560221e5de8
Hiroyuki Sato <hiroysato@gmail.com>
2021-05-02 21:10:18 +0900

    Rewrite code using v0.10 API/SPI

--- src/main/java/org/embulk/parser/NoneParserPlugin.java
+++ src/main/java/org/embulk/parser/NoneParserPlugin.java
@@ -1,46 +1,77 @@
 package org.embulk.parser;
 
-import org.embulk.config.Config;
-import org.embulk.config.ConfigDefault;
-import org.embulk.config.ConfigDiff;
+import org.embulk.util.config.Config;
+import org.embulk.util.config.ConfigDefault;
 import org.embulk.config.ConfigSource;
-import org.embulk.config.Task;
+import org.embulk.util.config.ConfigMapper;
+import org.embulk.util.config.ConfigMapperFactory;
+import org.embulk.util.config.Task;
 import org.embulk.config.TaskSource;
 import org.embulk.spi.ParserPlugin;
 import org.embulk.spi.FileInput;
 import org.embulk.spi.PageOutput;
 import org.embulk.spi.Schema;
-import org.embulk.spi.SchemaConfig;
+import org.embulk.util.config.TaskMapper;
+import org.embulk.util.config.units.SchemaConfig;
 
 import org.embulk.spi.Exec;
 import org.embulk.spi.PageBuilder;
-import org.embulk.spi.util.LineDecoder;
-import org.embulk.spi.ColumnConfig;
+import org.embulk.util.text.LineDecoder;
+import org.embulk.util.config.units.ColumnConfig;
+import org.embulk.util.text.LineDelimiter;
+import org.embulk.util.text.Newline;
+
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.embulk.spi.type.Types.STRING;
 
 public class NoneParserPlugin
         implements ParserPlugin
 {
+    private static final ConfigMapperFactory CONFIG_MAPPER_FACTORY = ConfigMapperFactory
+            .builder()
+            .addDefaultModules()
+            .build();
+    private static final ConfigMapper CONFIG_MAPPER = CONFIG_MAPPER_FACTORY.createConfigMapper();
+
     public interface PluginTask
-            extends Task, LineDecoder.DecoderTask //, TimestampParser.Task
+            extends Task
     {
         @Config("column_name")
         @ConfigDefault("\"payload\"")
         public String getColumnName();
+
+        // From org.embulk.spi.util.LineDecoder.DecoderTask
+        @Config("charset")
+        @ConfigDefault("\"utf-8\"")
+        public Charset getCharset();
+
+        // From org.embulk.spi.util.LineDecoder.DecoderTask
+        @Config("newline")
+        @ConfigDefault("\"CRLF\"")
+        public Newline getNewline();
+
+        // From org.embulk.spi.util.LineDecoder.DecoderTask.
+        @Config("line_delimiter_recognized")
+        @ConfigDefault("null")
+        Optional<LineDelimiter> getLineDelimiterRecognized();
+
     }
 
     @Override
     public void transaction(ConfigSource config, ParserPlugin.Control control)
     {
-        PluginTask task = config.loadConfig(PluginTask.class);
+        final PluginTask task = CONFIG_MAPPER.map(config, PluginTask.class);
         ArrayList<ColumnConfig> columns = new ArrayList<ColumnConfig>();
         final String columnName = task.getColumnName();
 
         columns.add(new ColumnConfig(columnName, STRING ,config));
 
         Schema schema = new SchemaConfig(columns).toSchema();
+
+        // TODO: Use task.toTaskSource() after dropping v0.9
         control.run(task.dump(), schema);
     }
 
@@ -48,8 +79,10 @@ public class NoneParserPlugin
     public void run(TaskSource taskSource, Schema schema,
             FileInput input, PageOutput output)
     {
-        PluginTask task = taskSource.loadTask(PluginTask.class);
-        LineDecoder lineDecoder = new LineDecoder(input,task);
+        final TaskMapper taskMapper = CONFIG_MAPPER_FACTORY.createTaskMapper();
+        final PluginTask task = taskMapper.map(taskSource, PluginTask.class);
+
+        LineDecoder lineDecoder = LineDecoder.of(input, task.getCharset(), task.getLineDelimiterRecognized().orElse(null));
         PageBuilder pageBuilder = new PageBuilder(Exec.getBufferAllocator(), schema, output);
         String line = null;
         final String columnName = task.getColumnName();
